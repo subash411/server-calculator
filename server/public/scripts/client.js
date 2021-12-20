00cd2c6014a346d59b2a69708fa6c0758a6ae149
Subash Khatri <subashkhatri44@gmail.com>
2021-12-19 18:01:00 -0600

    got stuck with server all day

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,5 @@
-$(document).ready(onReady);
-function onReady () {
+$(document).ready(readyNow);
+function readyNow () {
     console.log('On JQ');
     $('#equalBtn').on('click',onEquals);
     $('#clrBtn').on('click', clearInputs);
@@ -7,5 +7,20 @@ function onReady () {
     $('minusBtn').on('click', function () {numInput.operator = '-'}); 
     $('divideBtn').on('click', function () {numInput.operator = '/'});
     $('multiplyBtn').on('click', function () {numInput.operator = '*'}); 
+}
+
+function onEquals(event){
+    event.preventDefault();
+    numInput.number1 = $('#firstNumber').val('');
+    numInout.number2 = $('#secondeNumber').val('');
+    clearInputs();
+    $.ajax({
+        method: 'POST',
+        url: '/calculation',
+        data: numInp,
+    }).then((response) => {
+        render ();
+        console.log('Number submitted', response);
+    });
 }
 
--- server/server.js
+++ server/server.js
@@ -1,15 +1,43 @@
 const express = require('express');
+const app = express ();
+app.use(express.static('server/public'))
 const bodyParser = require('body-parser');
-const app = express();
-
-//app use
-
-app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-const port = 5000;
-app.listen(port, () =>{
-console.log('I\'m listening')
+
+//listen to 5000
+
+const PORT = 5001;
+app.listen(PORT, () =>{
+console.log('I\'m listening');
 });
+
+let calculation = [];
+
+app.post('/calculation', (req, res) => {
+const object = req.body;
+let operator = object.operator;
+let answer = 0;
+let number1 = Number(object.number1);
+let number2 = Number(object.number2);
+console.log('object');
+if (operator === '+'){
+    answer = number1+number2;
+
+} else if (operator === "-") {
+    answer = number1 - number2;
+
+  } else if (operator === "/") {
+    answer = number1 * number2;
+
+  } else if (operator === "*") {
+    answer = number1 / number2;
+
+  }
+  
+  
+
+})
+
 

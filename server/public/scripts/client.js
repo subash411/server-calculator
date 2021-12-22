bebca2f4316ecd3db0f9f1ddc265c7aaebaeaa2a
Subash Khatri <subashkhatri44@gmail.com>
2021-12-22 17:41:06 -0600

    calculation works, port 5000 works

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,26 +1,94 @@
-$(document).ready(readyNow);
-function readyNow () {
-    console.log('On JQ');
-    $('#equalBtn').on('click',onEquals);
-    $('#clrBtn').on('click', clearInputs);
-    $('plusBtn').on('click', function () {numInput.operator = '+'}); 
-    $('minusBtn').on('click', function () {numInput.operator = '-'}); 
-    $('divideBtn').on('click', function () {numInput.operator = '/'});
-    $('multiplyBtn').on('click', function () {numInput.operator = '*'}); 
+$(document).ready(onReady);
+function onReady(){
+    console.log('it is ready');
+
+    $("button").on("click", function (event) {
+        event.preventDefault();
+        operator = $(this).text();
+        console.log(operator);
+      });
+      $("#calcsForm").on("submit", addCalculation);
+
+    refresh();
+};
+function addCalculation(event) {
+  event.preventDefault();
+
+
+  let calculator = {
+    firstNum: $("#firstInput").val(),
+    secondNum: $("#secondInput").val(),
+    operators: operator,
+    total: 0,
+  };
+  console.log("calculator is ", calculator);
+
+  
+  $.ajax({
+    method: "POST",
+    url: "/calculator",
+   
+    data: calculator,
+  }).then((response) => {
+    console.log("POST response", response);
+
+    
+    refresh();
+  });
 }
 
-function onEquals(event){
-    event.preventDefault();
-    numInput.number1 = $('#firstNumber').val('');
-    numInout.number2 = $('#secondeNumber').val('');
-    clearInputs();
-    $.ajax({
-        method: 'POST',
-        url: '/calculation',
-        data: numInp,
-    }).then((response) => {
-        render ();
-        console.log('Number submitted', response);
-    });
+let operator;
+function ClearFields() {
+  $("#firstInput").val("");
+  $("#secondInput").val("");
 }
 
+
+function refresh() {
+ 
+  let ajaxOptions = {
+    method: "GET",
+    url: "/calculation",
+  };
+  $.ajax(ajaxOptions).then((response) => {
+    console.log("AJAX request complete!", response);
+    render(response);
+  });
+
+  
+}
+
+function render(calculation) {
+  // state to the DOM
+  $("#answers").empty();
+  for (let result of calculation) {
+    $("#answers").append(`
+            <li>
+                ${result.firstNum} ${result.operators}
+                ${result.secondNum} = ${result.total}
+                <div>
+                
+                </div>
+            </li>
+        `);
+    $("#Result").text(result.total);
+  }
+}
+
+function totalRender(total) {
+  $("#Result").text(`${total}`);
+}
+
+function total() {
+  let ajax = {
+    method: "GET",
+    url: "/result",
+  };
+  $.ajax(ajax).then((response) => {
+    console.log("AJAX request complete!", response);
+    totalRender(response);
+
+   
+  });
+
+}
--- server/server.js
+++ server/server.js
@@ -6,38 +6,57 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
-//listen to 5000
-
-const PORT = 5001;
-app.listen(PORT, () =>{
-console.log('I\'m listening');
-});
+// Global array to hold the data
 
 let calculation = [];
+let Total = 0;
 
-app.post('/calculation', (req, res) => {
-const object = req.body;
-let operator = object.operator;
-let answer = 0;
-let number1 = Number(object.number1);
-let number2 = Number(object.number2);
-console.log('object');
-if (operator === '+'){
-    answer = number1+number2;
+app.post("/calculator", (req, res) => {
+  console.log("in POST /calculator", req.body);
+  
+  let finalObj = {
+    firstNum: 0,
+    secondNum: 0,
+    operators: "+",
+    total: 0,
+  };
 
-} else if (operator === "-") {
-    answer = number1 - number2;
-
-  } else if (operator === "/") {
-    answer = number1 * number2;
-
-  } else if (operator === "*") {
-    answer = number1 / number2;
-
+  function math() {
+    console.log("in math", Number(req.body.firstNum));
+    if (req.body.operators === "+") {
+      Total = Number(req.body.firstNum) + Number(req.body.secondNum);
+    } else if (req.body.operators === "-") {
+      Total = Number(req.body.firstNum) - Number(req.body.secondNum);
+    } else if (req.body.operators === "/") {
+      Total = Number(req.body.firstNum) / Number(req.body.secondNum);
+    } else if (req.body.operators === "*") {
+      Total = Number(req.body.firstNum) * Number(req.body.secondNum);
+    }
   }
-  
-  
+  math();
 
-})
+  finalObj.firstNum = req.body.firstNum;
+  finalObj.secondNum = req.body.secondNum;
+  finalObj.operators = req.body.operators;
+  finalObj.total = Total;
 
+  calculation.push(finalObj);
 
+  // Send back  status
+  res.sendStatus(201);
+});
+
+app.get("/calculation", (req, res) => {
+  console.log("in GET /calculation");
+  res.send(calculation);
+});
+
+app.get("/result", (req, res) => {
+  console.log("in GET /result");
+  res.send(result);
+});
+
+const port = 5000;
+app.listen(port, () => {
+  console.log("I'm listening");
+});
